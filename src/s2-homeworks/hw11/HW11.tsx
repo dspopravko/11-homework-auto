32601450ecf1b4a46da992fb0a48d66fa333e849
dspopravko <demid1498@gmail.com>
2022-10-24 12:25:17 +0400

    hw11 styling

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -3,12 +3,26 @@ import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
 import {restoreState} from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
+import {createTheme, ThemeProvider} from "@mui/material";
+import {purple} from "@mui/material/colors";
 
 /*
 * 1 - передать значения в оба слайдера
 * 2 - дописать типы и логику функции change
 * 3 - сделать стили в соответствии с дизайном
 * */
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: purple[500],
+        },
+        secondary: {
+            main: '#00CC22',
+            light: '#8B8B8B'
+},
+    },
+});
 
 function HW11() {
     // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
@@ -25,34 +39,37 @@ function HW11() {
     }
 
     return (
-        <div id={'hw11'}>
-            <div className={s2.hwTitle}>Homework #11</div>
+        <ThemeProvider theme={theme}>
+            <div id={'hw11'}>
+                <div className={s2.hwTitle}>Homework #11</div>
 
-            <div className={s2.hw}>
-                <div className={s.container}>
-                    <div className={s.wrapper}>
-                        <span id={'hw11-value'} className={s.number}>{value1}</span>
-                        <SuperRange
-                            id={'hw11-single-slider'}
-                            onChange={(e)=>change(e, [1])}
-                            // сделать так чтоб value1 изменялось // пишет студент
-                            value={[value1]}
-                        />
-                    </div>
-                    <div className={s.wrapper}>
-                        <span id={'hw11-value-1'} className={s.number}>{value1}</span>
-                        <SuperRange
-                            id={'hw11-double-slider'}
-                            onChange={(e)=>change(e, [1, 2])}
-                            // сделать так чтоб value1/2 изменялось // пишет студент
-                            value={[value1, value2]}
-
-                        />
-                        <span id={'hw11-value-2'} className={s.number}>{value2}</span>
+                <div className={s2.hw}>
+                    <div className={s.container}>
+                        <div className={s.wrapper}>
+                            <span id={'hw11-value'} className={s.number}>{value1}</span>
+                            <SuperRange
+                                id={'hw11-single-slider'}
+                                onChange={(e) => change(e, [1])}
+                                // сделать так чтоб value1 изменялось // пишет студент
+                                value={[value1]}
+                                color={'secondary'}
+                            />
+                        </div>
+                        <div className={s.wrapper}>
+                            <span id={'hw11-value-1'} className={s.number}>{value1}</span>
+                            <SuperRange
+                                id={'hw11-double-slider'}
+                                onChange={(e) => change(e, [1, 2])}
+                                // сделать так чтоб value1/2 изменялось // пишет студент
+                                value={[value1, value2]}
+                                color={'secondary'}
+                            />
+                            <span id={'hw11-value-2'} className={s.number}>{value2}</span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </ThemeProvider>
     )
 }
 
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
-import {Slider, SliderProps} from '@mui/material'
+import {Slider, SliderProps, useTheme} from '@mui/material'
 
 const SuperRange: React.FC<SliderProps> = (props) => {
+    const theme = useTheme()
     return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
-        />
+            <Slider
+                sx={{ // стили для слайдера // пишет студент
+                    width: 147,
+                    color: theme.palette.secondary.main,
+                    '& .MuiSlider-thumb': {
+                        width: 20,
+                        height: 20,
+                        backgroundColor: theme.palette.secondary.main,
+                        border: `6px solid white`,
+                        borderRadius: '50%',
+                        outline: `1px solid ${theme.palette.secondary.main}`
+                    },
+                    '& .MuiSlider-rail': {
+                        backgroundColor: theme.palette.secondary.light
+                    }
+                }}
+                {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+            />
     )
 }
 
